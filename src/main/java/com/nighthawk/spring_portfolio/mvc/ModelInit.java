8dcc79650cfb08860cb30873d72bb214505e08ba
realethantran <etran8@gmail.com>
2023-11-30 19:24:31 -0800

    sorting algorithms to determine speed

--- src/main/java/com/nighthawk/spring_portfolio/mvc/ModelInit.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/ModelInit.java
@@ -2,8 +2,6 @@ package com.nighthawk.spring_portfolio.mvc;
 
 import com.nighthawk.spring_portfolio.mvc.person.Person;
 import com.nighthawk.spring_portfolio.mvc.person.PersonDetailsService;
-import com.nighthawk.spring_portfolio.mvc.car.Car;
-import com.nighthawk.spring_portfolio.mvc.car.CarJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -18,9 +16,6 @@ public class ModelInit {
     @Autowired
     private PersonDetailsService personService;
 
-    @Autowired
-    private CarJpaRepository carRepository;
-
     @Bean
     CommandLineRunner run() {
         return args -> {
@@ -30,15 +25,6 @@ public class ModelInit {
                 List<Person> personFound = personService.list(person.getName(), person.getEmail());
                 if (personFound.size() == 0) {
                     personService.save(person);
-                }
-            }
-
-            // Car database is populated with test data
-            Car[] carArray = Car.init(); // Assuming you have a static method init() in your Car class
-            for (Car car : carArray) {
-                Car existingCar = carRepository.save(car);
-                if (existingCar != null) {
-                    // Handle successfully saved car
                 }
             }
         };
