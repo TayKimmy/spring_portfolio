9ff0776e61bd75a0be4714e3e8d01b18b299144c
Tay Kim <taykimpro@gmail.com>
2023-12-04 18:56:36 -0800

    cors

--- src/main/java/com/nighthawk/spring_portfolio/mvc/car/FibonacciApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/car/FibonacciApiController.java
@@ -4,10 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/fibonacci")
 public class FibonacciApiController {
