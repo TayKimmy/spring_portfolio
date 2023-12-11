000f40bd599b536e80193a44836d2f9337ccdf06
Emaad-Mir <emaadidrismir@gmail.com>
2023-12-11 02:50:11 -0800

    fibonacci stuff

--- src/main/java/com/nighthawk/spring_portfolio/mvc/car/FibonacciApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/car/FibonacciApiController.java
@@ -2,8 +2,13 @@ package com.nighthawk.spring_portfolio.mvc.car;
 
 import org.springframework.web.bind.annotation.*;
 
+import com.nighthawk.spring_portfolio.mvc.betting.Betting;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -87,6 +92,50 @@ public class FibonacciApiController {
             return a;
         }
     }
+
+    @GetMapping("/bet")
+    public Map<String, Object> betOnFibonacciRace(@RequestParam(required = true) int betAmount,
+                                                  @RequestParam(required = true) int startingPoints) {
+        Betting game = new Betting(startingPoints);
+    
+        Map<String, Integer> speeds = getAlgorithmSpeeds(null);
+    
+        // Randomly select a Fibonacci algorithm
+        List<String> algorithms = new ArrayList<>(speeds.keySet());
+        String selectedAlgorithm = algorithms.get(new Random().nextInt(algorithms.size()));
+    
+        String fastestAlgorithm = speeds.entrySet().stream()
+                .min(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse("none");
+    
+        boolean isGuessCorrect = selectedAlgorithm.equals(fastestAlgorithm);
+    
+        game.placeBet(betAmount, isGuessCorrect);
+    
+        Map<String, Object> result = new HashMap<>();
+        result.put("message", game.getResultMessage());
+        result.put("newScore", game.getPoints());
+        result.put("selectedAlgorithm", selectedAlgorithm);
+        result.put("fastestAlgorithm", fastestAlgorithm);
+    
+        Map<String, String> fibonacciFacts = getFibonacciFacts();
+        result.put("funFact", fibonacciFacts.get(fastestAlgorithm));
+    
+        return result;
+    }
+    
+    private Map<String, String> getFibonacciFacts() {
+        Map<String, String> facts = new HashMap<>();
+        facts.put("forLoopFibonacci", "For Loop Fibonacci is efficient for small sequences but becomes less efficient as the sequence grows due to its linear time complexity.");
+        facts.put("whileLoopFibonacci", "While Loop Fibonacci is similar to the for-loop version but uses a while loop, making it slightly more versatile in certain contexts.");
+        facts.put("recursionFibonacci", "Recursion Fibonacci is elegant but can be less efficient due to its exponential time complexity, especially for large sequences.");
+        facts.put("matrixFibonacci", "Matrix Fibonacci uses matrix multiplication to calculate Fibonacci numbers, providing a more efficient approach for larger sequences.");
+        return facts;
+    }
+    
+
+    
 
     @GetMapping("/speeds")
     public Map<String, Integer> getAlgorithmSpeeds(@RequestParam(required = false) Integer length) {
