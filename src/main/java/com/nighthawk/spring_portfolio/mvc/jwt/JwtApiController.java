59a842b3e2ca0fe9b140100b18250accff517714
Tay Kim <taykimpro@gmail.com>
2024-01-04 09:51:45 -0800

    ii

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
@@ -44,6 +44,7 @@ public class JwtApiController {
 			.sameSite("None; Secure")
 			// .domain("example.com") // Set to backend domain
 			.build();
+		System.out.println(tokenCookie.toString());
 		return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, tokenCookie.toString()).build();
 	}
 
